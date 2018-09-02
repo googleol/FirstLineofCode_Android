978960c984963b4c64a1ac6b999b32254fc5fa79
googleol <mzwangpost@outlook.com>
2018-09-02 11:54:09 +0800

    Add menu and overide member function OnCreateOptionsMenu and OnOptionsItemSelected

--- FirstActivity/app/src/main/java/cn/iecas/firstactivity/MainActivity.java
+++ FirstActivity/app/src/main/java/cn/iecas/firstactivity/MainActivity.java
@@ -2,11 +2,37 @@ package cn.iecas.firstactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater f = getMenuInflater();
+        f.inflate(R.menu.men,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Options:
+                Toast.makeText(this,"You clicked Options Menu",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Settings:
+                Toast.makeText(this,"You clicked Settings Menu",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Help:
+                Toast.makeText(this,"You clicked Hrlp Menu",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"Hello Buddy",
                         Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
+
     }
+
 }
